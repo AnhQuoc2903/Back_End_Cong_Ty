00a0ba9361b5407b184cae3a3f0c9e52f5d9647e
AnhQuoc2903 <133018919+AnhQuoc2903@users.noreply.github.com>
2025-12-19 12:20:30 +0700

    remove dist from git and ignore it

--- dist/models/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./permission.model");
-require("./role.model");
-require("./user.model");
-require("./artifactCategory.model");
-require("./artifact.model");
-require("./artifactTransaction.model");
